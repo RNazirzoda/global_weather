d38cfb5716558ae6737971adf39ca9fb57487af2
Rahimjon Nazirzoda <raxajan.tj@gmail.com>
2025-03-17 22:54:09 +0500

    Добавлены SQL-скрипты: schema.sql, views.sql, queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -28,3 +28,24 @@ group by
     year_month
 order by 
     year_month;
+
+-- moving average temperature (оконная функция)
+select 
+    location_id,
+    last_updated,
+    temperature_celsius,
+    avg(temperature_celsius) over (
+        partition by location_id 
+        order by last_updated 
+        rows between 6 preceding 
+        and current row
+        ) as moving_avg_temp
+from weather;
+
+-- find locations with above average temperatures (подзапрос)
+select location_name, temperature_celsius
+from weather
+where temperature_celsius > (
+    select avg(temperature_celsius) 
+    from weather
+);
